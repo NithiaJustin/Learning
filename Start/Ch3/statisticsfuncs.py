6c23393445677a11e8e7a5bc5ffdb049dd2529df
IfeanyiOsuji <59509350+IfeanyiOsuji@users.noreply.github.com>
2024-05-17 12:01:05 +0000

    implemented solutions for the exercises

--- Start/Ch3/statisticsfuncs.py
+++ Start/Ch3/statisticsfuncs.py
@@ -14,9 +14,13 @@ summer_months = [d for d in weatherdata if any([month in d['date'] for month in 
 print(f"Data for {len(summer_months)} summer days")
 
 # TODO: calculate the mean for both min and max temperatures
-
+max_temp = [d['tmax'] for d in summer_months]
+min_temp = [d['tmin'] for d in summer_months]
+print(mean_tmax:=statistics.mean(max_temp))
+print(mean_tmin:=statistics.mean(min_temp))
 
 # TODO: calculate the median values for min and max temperatures
-
+print(statistics.median(max_temp))
+print(statistics.median(min_temp))
 
 # TODO: use the standard deviation function to find outlier temperatures
